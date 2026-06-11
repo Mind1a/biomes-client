611a4092347146346ba65845a4d927d5a88aa448
Alievi1111 <alieviali1111@gmail.com>
2026-06-11 12:04:31 +0400

    feature/404
    create 404 Error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import { tbcContracticaCaps } from "@/public/assets/fonts/fonts";
+
 export default function NotFound() {
   return (
-    <div className="text-red-800 flex min-h-screen items-center justify-center text-6xl">
-      404
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-[#F3F3F3] ">
+      <Image
+        src="/Frame.svg"
+        alt="404 Illustration"
+        width={682}
+        height={498}
+        priority
+      />
+
+      <h1
+        className={`${tbcContracticaCaps.className} mt-10 text-center text-[32px] font-bold text-[#666666]`}
+      >
+        შედეგები ვერ მოიძებნა
+      </h1>
+
+      <Link href="/" className="mt-4 flex items-center gap-2">
+        <Image src="/arrow.svg" alt="Back Arrow" width={22} height={22} />
+
+        <span
+          className={`${tbcContracticaCaps.className} text-[22px] font-medium text-black`}
+        >
+          მთავარი გვერდი
+        </span>
+      </Link>
+    </main>
   );
 }
