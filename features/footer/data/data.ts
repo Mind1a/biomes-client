550a616f1991ecbe57a1806ec5c017dc910fa149
Elene Pitskhelauri <eleneficxelauri01@gmail.com>
2026-06-12 16:26:23 +0400

    feat: move footer to feature and add data-driven content

---
+++ features/footer/data/data.ts
@@ -0,0 +1,29 @@
+export const footerData = {
+  copyright: "© ყველა უფლება დაცულია",
+  mainLogo: {
+    href: "/",
+    src: "/footer/biomes-logo.png",
+    alt: "ბიომები",
+    ariaLabel: "მთავარ გვერდზე გადასვლა",
+    width: 220,
+    height: 48,
+  },
+  partners: [
+    {
+      href: "https://iliauni.edu.ge",
+      src: "/footer/ilia-logo.png",
+      alt: "Ilia State University",
+      ariaLabel: "Ilia State University website",
+      width: 71,
+      height: 68,
+    },
+    {
+      href: "https://unilab.iliauni.edu.ge",
+      src: "/footer/unilab-logo.png",
+      alt: "Unilab",
+      ariaLabel: "Unilab website",
+      width: 51,
+      height: 69,
+    },
+  ],
+};
